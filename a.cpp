c40e34388930c8bc85c5a40d72b5f19ee8c4c490
teacherZhang <1476203673@qq.com>
2023-04-09 16:08:21 +0800

    添加了a.cpp

---
+++ a.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define int128 __int128
+typedef pair<int,int> PII;
+const int N=1e6+10;
+
+void solve(){
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t=1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
